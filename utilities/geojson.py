0fb06e295e900840e29ce694d248f851ca5782c7
Lior Sinai <sinailior@gmail.com>
2023-12-02 15:36:28 +0200

    feature: flexible number of largest areas to keep per multipolygon

--- utilities/geojson.py
+++ utilities/geojson.py
@@ -15,7 +15,7 @@ def filter_features_by_list(shape_data, prop_name, prop_list):
             features.append(feature)
     return features
 
-def get_polygons(features, identifier='shapeName', mainland=False):
+def get_polygons(features, identifier='shapeName', keep_top=-1):
     shapes = {}
     for idx, feat in enumerate(features):
         geometry_type = feat['geometry']['type']
@@ -25,21 +25,23 @@ def get_polygons(features, identifier='shapeName', mainland=False):
         if geometry_type== 'Polygon':
             coords = feat['geometry']['coordinates'][0]
             polygon = Polygon(coords)
-            shapes[id] = polygon
         elif geometry_type== 'MultiPolygon':
             polygons = []
             areas = []
             for coords in feat['geometry']['coordinates']:
                 polygon = Polygon(coords[0])
                 polygons.append(polygon)
-                areas.append(polygon.area)
-            if mainland:
-                # assume mainland is the largest area and all others are islands
-                idx = np.argmax(areas)
-                shapes[id] = polygons[idx]
+            if keep_top > 0:
+                areas = [p.area for p in polygons]
+                if keep_top == 1:
+                    idx = np.argmax(areas)
+                    polygon = polygons[idx]
+                else:
+                    idxs = np.argsort(areas)[:(-keep_top - 1):-1]
+                    polygon = MultiPolygon([polygons[i] for i in idxs])
             else:
-                multipolygon = MultiPolygon(polygons)
-                shapes[id] = multipolygon
+                polygon = MultiPolygon(polygons)
         else:
             print(f'Feature at idx {idx} is of type {geometry_type} is not supported')
+        shapes[id] = polygon
     return shapes
