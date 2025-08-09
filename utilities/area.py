0c2b4a42f08468e7d7e27ebea335f05ab5f4d718
Lior Sinai <sinailior@gmail.com>
2025-08-09 15:38:10 +0200

    feature: more support for multi-polygons

--- utilities/area.py
+++ utilities/area.py
@@ -1,7 +1,9 @@
-from shapely import Polygon
+from shapely import MultiPolygon, Polygon
 import numpy as np
 import rasterio
 import rasterio.mask
+from typing import List, Union
+
 
 # https://earth-info.nga.mil/index.php?dir=wgs84&action=wgs84
 MAJOR_SEMI_AXIS_WGS84 = 6_378_137 # metres
@@ -9,6 +11,7 @@ FLATTENING_WGS84 = 1 / 298.257223563
 MINOR_SEMI_AXIS_WGS84 = MAJOR_SEMI_AXIS_WGS84 * (1 - FLATTENING_WGS84)
 EARTH_AUTHALIC_RADIUS =  6_371_007.2 # metres. Radius of a sphere with same surface area as the WSG-84 Earth spheroid.
 EARTH_RADIUS = EARTH_AUTHALIC_RADIUS
+
 
 def cylindrical_projection(latitudes, longitudes):
     latitudes = np.array(latitudes)
@@ -18,11 +21,13 @@ def cylindrical_projection(latitudes, longitudes):
     new_poly = Polygon(zip(xs, ys))
     return new_poly
 
+
 # https://stackoverflow.com/questions/4681737/how-to-calculate-the-area-of-a-polygon-on-the-earths-surface-using-python
 def calc_area(latitudes, longitudes):
     poly_cylindrical = cylindrical_projection(latitudes, longitudes)
     area = EARTH_RADIUS * EARTH_RADIUS * poly_cylindrical.area # metres^2
     return area
+
 
 def show_stats(population_data, polygons):
     clipped_img, transform = rasterio.mask.mask(population_data, polygons, crop=True)
@@ -39,21 +44,42 @@ def show_stats(population_data, polygons):
     density = population_count / (area/1e6)
     print(f'density:    {density:.2f} people/km^2')
 
-def get_density_per_area(population_data, polygons):
-    n = len(polygons)
+
+def density_per_polygon(population_data: rasterio.io.DatasetReader, polygon: Polygon):
+    clipped_img, transform = rasterio.mask.mask(population_data, [polygon], crop=True)
+    clipped_img[clipped_img < 0] = 0 
+    population_count = clipped_img.sum() 
+    longs, lats = polygon.exterior.coords.xy
+    area = calc_area(lats, longs) # metres^2 
+    density = population_count / (area/1e6)
+    return density, population_count, area
+
+
+def get_density_per_area(
+        population_data: rasterio.io.DatasetReader,
+        shapes: List[Union[Polygon, MultiPolygon]]
+    ):
+    n = len(shapes)
     densities = np.zeros(n)
     population_counts = np.zeros(n)
     areas = np.zeros(n)
-    for idx, polygon in enumerate(polygons):
-        clipped_img, transform = rasterio.mask.mask(population_data, [polygon], crop=True)
-        clipped_img[clipped_img < 0] = 0 
-        population_count = clipped_img.sum() 
-        longs, lats = polygon.exterior.coords.xy
-        area = calc_area(lats, longs) # metres^2 
-        densities[idx] = population_count / (area/1e6) # people / km^2
-        population_counts[idx] = population_count
-        areas[idx] = area
+    for idx, shape in enumerate(shapes):
+        if isinstance(shape, Polygon):
+            polygons = [shape]
+        elif isinstance(shape, MultiPolygon):
+            polygons = shape.geoms
+        else:
+            type_ = type(shape)
+            raise Exception(f"type {type_} is not supported")
+        areas[idx] = 0.0
+        population_counts[idx] = 0.0
+        for polygon in polygons:
+            density, population_count, area = density_per_polygon(population_data, polygon)
+            population_counts[idx] += population_count
+            areas[idx] += area
+        densities[idx] = population_counts[idx] / (areas[idx]/1e6)
     return densities, population_counts, areas
+
 
 def get_scales(shape, bounding_polygon):
     height, width = shape
--- utilities/geojson.py
+++ utilities/geojson.py
@@ -8,6 +8,7 @@ def filter_features(shape_data, prop_name, prop_val):
             features.append(feature)
     return features
 
+
 def filter_features_by_list(shape_data, prop_name, prop_list):
     features = []
     for feature in shape_data['features']:
@@ -15,33 +16,40 @@ def filter_features_by_list(shape_data, prop_name, prop_list):
             features.append(feature)
     return features
 
+
+def convert_dict_to_shapely(geometry: dict, keep_top = -1):
+    geometry_type = geometry['type']
+    if geometry_type == 'Polygon':
+        coords = geometry['coordinates']
+        holes = coords[1:] if len(coords) > 1 else None
+        polygon = Polygon(coords[0], holes=holes)
+    elif geometry_type== 'MultiPolygon':
+        polygons = []
+        for coords in geometry['coordinates']:
+            holes = coords[1:] if len(coords) > 1 else None
+            polygon = Polygon(coords[0], holes=holes)
+            polygons.append(polygon)
+        if keep_top > 0:
+            areas = [p.area for p in polygons]
+            if keep_top == 1:
+                idx = np.argmax(areas)
+                polygon = polygons[idx]
+            else:
+                idxs = np.argsort(areas)[:(-keep_top - 1):-1]
+                polygon = MultiPolygon([polygons[i] for i in idxs])
+        else:
+            polygon = MultiPolygon(polygons)
+    else:
+        raise Exception(f"Type not supported: {geometry_type}")
+    return polygon
+
+
 def get_polygons(features, identifier='shapeName', keep_top=-1):
     shapes = {}
     for idx, feat in enumerate(features):
-        geometry_type = feat['geometry']['type']
         id = feat['properties'][identifier]
         if (id in shapes):
             raise Exception('Duplicate id: ' + id)
-        if geometry_type== 'Polygon':
-            coords = feat['geometry']['coordinates'][0]
-            polygon = Polygon(coords)
-        elif geometry_type== 'MultiPolygon':
-            polygons = []
-            areas = []
-            for coords in feat['geometry']['coordinates']:
-                polygon = Polygon(coords[0])
-                polygons.append(polygon)
-            if keep_top > 0:
-                areas = [p.area for p in polygons]
-                if keep_top == 1:
-                    idx = np.argmax(areas)
-                    polygon = polygons[idx]
-                else:
-                    idxs = np.argsort(areas)[:(-keep_top - 1):-1]
-                    polygon = MultiPolygon([polygons[i] for i in idxs])
-            else:
-                polygon = MultiPolygon(polygons)
-        else:
-            print(f'Feature at idx {idx} is of type {geometry_type} is not supported')
+        polygon = convert_dict_to_shapely(feat['geometry'], keep_top=keep_top)
         shapes[id] = polygon
     return shapes
