67413f90fded3ad860fa645118430ed4c49ab8a7
Lior Sinai <sinailior@gmail.com>
2025-12-15 10:40:19 +0200

    feature: area function updates: - Logging. - Calculate great circle distance. - Catch error in density calculation: set population to 0 with a warning if there is no intersection. Break: swap latitudes with longitudes to match (x, y) convention.

--- utilities/area.py
+++ utilities/area.py
@@ -1,8 +1,11 @@
-from shapely import MultiPolygon, Polygon
+import logging
 import numpy as np
 import rasterio
 import rasterio.mask
+from shapely import MultiPolygon, Polygon
 from typing import List, Union
+
+logger = logging.getLogger(__name__)
 
 
 # https://earth-info.nga.mil/index.php?dir=wgs84&action=wgs84
@@ -13,23 +16,66 @@ EARTH_AUTHALIC_RADIUS =  6_371_007.2 # metres. Radius of a sphere with same surf
 EARTH_RADIUS = EARTH_AUTHALIC_RADIUS
 
 
-def cylindrical_projection(latitudes: List[float], longitudes: List[float]):
-    latitudes = np.array(latitudes)
-    longitudes = np.array(longitudes)
-    ys = np.sin(latitudes * np.pi / 180)
-    xs = longitudes * np.pi / 180
+# -------------------------------------------#
+#      Great circle distance on a sphere
+# -------------------------------------------#
+
+def hav(angle: float):
+    return (1 - np.cos(angle))/2
+
+
+def haversine_formula(long1: float, lat1: float, long2: float, lat2: float):
+    """
+    Haversine formula for the central angle between two points on a sphere.
+    Values must be in radians.
+    """
+    hav_theta = hav(lat2 - lat1) + np.cos(lat1) * np.cos(lat2) * hav(long2 - long1)
+    theta = np.arccos(1 - 2 * hav_theta)
+    return theta
+
+
+def calc_great_circle_distance(point1: tuple, point2: tuple, radius: float = EARTH_AUTHALIC_RADIUS):
+    """
+    Calculate the great circle distance betweeb two points on a sphere.
+    Points are in (longitude, latitude) pairs in degrees.
+    By default, the radius is 6,371,007 metres and the result is returned in metres.
+    """
+    theta = haversine_formula(np.deg2rad(point1[0]), np.deg2rad(point1[1]), np.deg2rad(point2[0]), np.deg2rad(point2[1]))
+    distance = radius * theta
+    return distance
+
+
+# -------------------------------------------#
+#                Area on a sphere
+# -------------------------------------------#
+
+
+def cylindrical_projection(longitudes: List[float], latitudes: List[float]):
+    latitudes = np.deg2rad(np.array(latitudes))
+    longitudes = np.deg2rad(np.array(longitudes))
+    ys = np.sin(latitudes)
+    xs = longitudes
     new_poly = Polygon(zip(xs, ys))
     return new_poly
 
 
-# https://stackoverflow.com/questions/4681737/how-to-calculate-the-area-of-a-polygon-on-the-earths-surface-using-python
 def calc_area(
-        latitudes: List[float], longitudes: List[float],
+        longitudes: List[float], latitudes: List[float],
         radius: float = EARTH_RADIUS
     ):
-    poly_cylindrical = cylindrical_projection(latitudes, longitudes)
+    """
+    Calculate the area of an object on the surface of a sphere by projecting it on a cylinder.
+    The longitudes and latitudes must be in degrees.
+    By default, the radius is 6,371,007 metres and the result is returned in metres^2.
+    """
+    # https://stackoverflow.com/questions/4681737/how-to-calculate-the-area-of-a-polygon-on-the-earths-surface-using-python
+    poly_cylindrical = cylindrical_projection(longitudes, latitudes)
     area = radius * radius * poly_cylindrical.area # metres^2
     return area
+
+# -------------------------------------------#
+#            Density calculations
+# -------------------------------------------#
 
 
 def show_stats(
@@ -46,7 +92,7 @@ def show_stats(
         polygons = geometry.geoms if isinstance(geometry, MultiPolygon) else [geometry]
         for polygon in polygons:
             longs, lats = polygon.exterior.coords.xy
-            area += calc_area(lats, longs, radius=radius) # metres^2
+            area += calc_area(longs, lats, radius=radius) # metres^2
     print(f'population: {population_count/1e6:.2f} million')
     print(f'max:        {population_max:.0f} people / pixel')
     print(f'area:       {area/1e6:.2f} km^2')
@@ -59,11 +105,15 @@ def density_per_polygon(
         polygon: Polygon,
         radius: float = EARTH_RADIUS
     ):
-    clipped_img, transform = rasterio.mask.mask(population_data, [polygon], crop=True)
-    clipped_img[clipped_img < 0] = 0 
-    population_count = clipped_img.sum() 
+    try:
+        clipped_img, transform = rasterio.mask.mask(population_data, [polygon], crop=True)
+        clipped_img[clipped_img < 0] = 0 
+        population_count = clipped_img.sum() 
+    except ValueError as e:
+        logger.warning(f"{e} Setting population count to 0.")
+        population_count = 0
     longs, lats = polygon.exterior.coords.xy
-    area = calc_area(lats, longs, radius=radius) # metres^2 
+    area = calc_area(longs, lats, radius=radius) # metres^2 
     density = population_count / (area/1e6)
     return density, population_count, area
 
@@ -92,7 +142,7 @@ def get_density_per_area(
                 population_data, polygon, radius=radius)
             population_counts[idx] += population_count
             areas[idx] += area
-        densities[idx] = population_counts[idx] / (areas[idx]/1e6)
+        densities[idx] = population_counts[idx] / (areas[idx]/1e6) # convert to km2
     return densities, population_counts, areas
 
 
